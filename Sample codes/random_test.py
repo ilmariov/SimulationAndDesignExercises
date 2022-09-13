49f9ab499b9e20ef77fde730aa0b5e9633511689
Mario Vega <malbertovega@gmail.com>
2022-09-12 20:15:47 -0500

    simulation repo created
    sample codes folder added

---
+++ Sample codes/random_test.py
@@ -0,0 +1,11 @@
+from random import randrange, random
+
+def randrange_num():
+    ran_num = randrange (1,6)
+    print(ran_num)
+
+def random_num():
+    ran_num = random()
+    print(ran_num)
+
+if __name__=='__main__': random_num()
