1925e25b26341068850508de52ddd0ff41ec60f8
Mario Vega <malbertovega@gmail.com>
2022-09-13 23:07:32 -0500

    testing random functions

--- randTest1.py
+++ randTest1.py
@@ -1,6 +1,6 @@
 # A program to calculate a random float in the range -0.5 - 0.5
 
-from random import random, randrange
+from random import random, randrange, randint, uniform
 
 def main():
     num = randrange(-1,3,2)*random()
@@ -8,4 +8,14 @@ def main():
         num = randrange(-1,3,2)*random()
     print(num)
 
-if __name__== '__main__': main()
+def sixSidedDie():
+    print(randint(1,6))
+
+def sumDices():
+    sum = randint(1,6) + randint(1,6)
+    print(sum)
+
+def randFloat():
+    print(uniform(-0.5,0.5))
+
+if __name__== '__main__': randFloat()
