27b7dbb7baf36e10e9537d38976e6267ccfd6432
Mario Vega <malbertovega@gmail.com>
2022-09-17 16:34:24 -0500

    pending printing results

--- E9.08-blackjack.py
+++ E9.08-blackjack.py
@@ -22,29 +22,17 @@ def simNGames(n):
 def simOneGame():
     # This function checks if the dealer bust or not
     # it prints True if he busts, False otherwise.
-    # checker = None
+    checker = False
     pcard1, pcard2, dcard1, dcard2 = deal()
-    print(pcard1, pcard2)
     player_hand = hit(pcard1, pcard2)
-    print(player_hand)
-    print('-------------------------')
-    print(dcard1, dcard2)
     dealer_hand = hit(dcard1, dcard2)
-    print(dealer_hand)
-
-def hasAce(card1, card2):
-    return (card1==1 or card2==1)
-
-def hasAce2(cards):
-    checker = None
-    for i in range(len(cards)):
-        if int(cards[i]) == 1:
-            checker = True
-            break
+    if player_hand == 21:
+        checker = False
+    elif dealer_hand > 21:
+        checker = True
+        if player_hand > 21:
+            checker = False
     return checker
-
-def blackjack(c1, c2):
-    return ((c1==1 and c2==10) or (c1==10 and c2==1))
 
 def deal():
     pcard1 = randint(1,10)
@@ -52,37 +40,44 @@ def deal():
     dcard1 = randint(1,10)
     dcard2 = randint(1,10)
     return pcard1, pcard2, dcard1, dcard2
-
-'''def firstHand():
-    # returns 1:player wins, 2:dealer wins, 3:game continues, 4:are even
-    pcard1, pcard2, dcard1, dcard2 = deal()
-    player_hand = pcard1 + pcard2
-    dealer_hand = dcard1 + dcard2
-    if hasAce(pcard1, pcard2) or hasAce(dcard1, dcard2):
-        if hasAce(pcard1, pcard2) and player_hand==11:
-            if hasAce(dcard1, dcard2) and dealer_hand==11:
-                return 4
-            else:
-                return 1
-        elif '''
-        
+    
 def hit(card1, card2):
     hand = card1 + card2
     cards = [card1, card2]    
     if not blackjack(card1, card2):
+        switch = 0
         while (hand < 21):
             new_card = randint(1,10)
             cards.append(new_card)
             print(new_card)
-            if (hasAce2(cards) and (7 <= hand <= 11)):
-                hand = hand + new_card + 10
-                if hand > 21:
-                    hand = hand -10
+            if hasAce(cards):
+                if hand <= 11:
+                    hand = hand + new_card + 10
+                    switch = 1
+                    if hand > 21:
+                        hand = hand - 10
+                        switch = 0
+                elif (switch==1 and (hand + new_card > 21)):
+                    hand = hand + new_card -10
+                    switch = 0
+                else:
+                    hand = hand + new_card
             else:
                 hand = hand + new_card
     else:
         hand = hand + 10
     return hand
+
+def blackjack(c1, c2):
+    return ((c1==1 and c2==10) or (c1==10 and c2==1))
+
+def hasAce(cards):
+    checker = None
+    for i in range(len(cards)):
+        if int(cards[i]) == 1:
+            checker = True
+            break
+    return checker
 
 
 if __name__=='__main__' : simOneGame()
