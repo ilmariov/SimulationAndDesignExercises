aec42870d08fe0b8bd03cc9d7c1235d7af9035f0
Mario Vega <malbertovega@gmail.com>
2022-09-18 10:15:14 -0500

    blackjack program tested

--- E9.08-blackjack.py
+++ E9.08-blackjack.py
@@ -3,7 +3,7 @@ from random import randint
 def main():
     printIntro()
     n = int(input('\nHow many games to simulate? '))
-    busts = simNGames()
+    busts = simNGames(n)
     printProb(n, busts)
 
 def printIntro():
@@ -26,12 +26,21 @@ def simOneGame():
     pcard1, pcard2, dcard1, dcard2 = deal()
     player_hand = hit(pcard1, pcard2)
     dealer_hand = hit(dcard1, dcard2)
-    if player_hand == 21:
+    if player_hand==21 or dealer_hand==21:
         checker = False
-    elif dealer_hand > 21:
-        checker = True
-        if player_hand > 21:
-            checker = False
+    elif (player_hand > dealer_hand):
+        last_dcard = randint(1,10)
+        if dealer_hand + last_dcard > 21:
+            checker = True
+    else:
+        while (player_hand <= dealer_hand) and (player_hand < 21) and (dealer_hand < 21):
+            last_pcard = randint(1,10)
+            player_hand = player_hand + last_pcard
+            if player_hand < 21:
+                last_dcard = randint(1,10)
+                dealer_hand = dealer_hand + last_dcard
+                if dealer_hand > 21:
+                    checker = True
     return checker
 
 def deal():
@@ -45,28 +54,35 @@ def hit(card1, card2):
     hand = card1 + card2
     cards = [card1, card2]    
     if not blackjack(card1, card2):
-        switch = 0
-        while (hand < 21):
-            new_card = randint(1,10)
-            cards.append(new_card)
-            print(new_card)
-            if hasAce(cards):
-                if hand <= 11:
-                    hand = hand + new_card + 10
-                    switch = 1
-                    if hand > 21:
-                        hand = hand - 10
-                        switch = 0
-                elif (switch==1 and (hand + new_card > 21)):
-                    hand = hand + new_card -10
-                    switch = 0
-                else:
-                    hand = hand + new_card
-            else:
-                hand = hand + new_card
+        new_card = 0
+        while ((hand - new_card < 17) or (hand - new_card >= 21)):
+            hand, new_card = getClose(cards, hand)
+        hand = hand - new_card
     else:
         hand = hand + 10
     return hand
+
+def getClose(cards,hand):
+    switch = 0
+    new_card = randint(1,10)
+    while (hand < 21):
+        new_card = randint(1,10)
+        cards.append(new_card)
+        if hasAce(cards):
+            if hand <= 11:
+                hand = hand + new_card + 10
+                switch = 1
+                if hand > 21:
+                    hand = hand - 10
+                    switch = 0
+            elif (switch==1 and (hand + new_card > 21)):
+                hand = hand + new_card -10
+                switch = 0
+            else:
+                hand = hand + new_card
+        else:
+            hand = hand + new_card
+    return hand, new_card
 
 def blackjack(c1, c2):
     return ((c1==1 and c2==10) or (c1==10 and c2==1))
@@ -79,5 +95,9 @@ def hasAce(cards):
             break
     return checker
 
+def printProb(n, busts):
+    print('\nAfter simulating {} games of blackjack, the estimated'.format(n))
+    print('probability that the dealer will bust is: {0:0.1%}'.format(busts/n))
 
-if __name__=='__main__' : simOneGame()
+
+if __name__=='__main__' : main()
